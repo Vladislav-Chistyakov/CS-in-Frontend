a00eb025e4818f143d32477caec5a9eb0ff773aa
Vladislav-Chistyakov <vladischis@gmail.com>
2026-04-26 15:57:52 +0300

    7: person

--- 7/struct.js
+++ 7/struct.js
@@ -1,54 +1,59 @@
 export class Struct {
+  constructor(scheme) {
+    let totalLength = 0;
 
-  constructor(schema) {
-    let totalLength = 0
+    this.scheme = new Map(
+      Object.entries(scheme).flatMap(([key, type]) => {
+        const alignment = this.#getAlignment(totalLength, type.alignment ?? 1);
 
-    this.schema = new Map(
-      Object.entries(schema).flatMap(([key, type]) => {
-        const alignment = this.#getAlignment(totalLength, type.alignment ?? 1)
-
-        const res = []
+        const res = [];
 
         if (alignment !== 0) {
           res.push([
             Symbol('Alignment'),
+
             {
               byteLength: alignment,
               init: () => {
                 return {
                   get: () => 0,
-                  set: (_value) = {},
-                }
+                  set: (_value) => {}
+                };
               }
             }
-          ])
-          totalLength += alignment
+          ]);
+
+          totalLength += alignment;
         }
 
-        res.push([key, {
-          byteLength: type.byteLength,
-          init: type.init.bind(type)
-        }])
+        res.push([
+          key,
 
-        totalLength += type.byteLength
+          {
+            byteLength: type.byteLength,
+            init: type.init.bind(type)
+          }
+        ]);
 
-        return res
-      }),
+        totalLength += type.byteLength;
+
+        return res;
+      })
     )
 
-    this.byteLength = totalLength
-
-    console.log(this.schema)
-    console.log(this.byteLength)
+    this.byteLength = totalLength;
   }
 
   create(data, buffer = new ArrayBuffer(this.byteLength), offset = 0) {
-    const view = new StructView(buffer, this.byteLength, offset)
-    this.schema.forEach((type, key) => {
-      const {get, set} = type.init(buffer, offset)
+    const view = new StructView(buffer, this.byteLength, offset);
+
+    this.scheme.forEach((type, key) => {
+      const {get, set} = type.init(buffer, offset);
+
+      offset += type.byteLength;
 
       if (typeof key !== 'symbol') {
-        set(data[key])
+        set(data[key]);
 
         Object.defineProperty(view, key, {
           enumerable: true,
@@ -56,110 +61,89 @@ export class Struct {
           get,
           set,
         })
-
-        offset += type.byteLength
       }
-    })
-    return view
+    });
+
+    return view;
   }
 
   from(buffer, offset = 0) {
-    const view = new StructView(buffer, this.byteLength, offset)
+    const view = new StructView(buffer, this.byteLength, offset);
 
-    this.schema.forEach((type, key) => {
+    this.scheme.forEach((type, key) => {
+      const currentOffset = offset;
+      offset += type.byteLength;
+
       let accessors;
-
-      const currentOffset = offset
-
-      offset += type.byteLength
 
       if (typeof key !== 'symbol') {
         Object.defineProperty(view, key, {
           enumerable: true,
           configurable: true,
+
           get: () => init().get(),
+
           set: (value) => {
-            init().set()
+            init().set(value);
           },
         })
+      }
 
-
-        function init() {
-          if (accessors == null) {
-            accessors = type.init(buffer, currentOffset)
-          }
-
-          return accessors
+      function init() {
+        if (accessors == null) {
+          accessors = type.init(buffer, currentOffset);
         }
 
+        return accessors
       }
-    })
+    });
 
-    return view
+    return view;
   }
 
-  init(buffer, offset = 0) {
-    let view = this.from(buffer, offset)
+  init(buffer, offset) {
+    let view = this.from(buffer, offset);
+
     return {
       get: () => view,
+
       set: (data) => {
-        view = this.create(data, buffer, offset)
+        view = this.create(data, buffer, offset);
       }
-    }
+    };
   }
 
   #getAlignment(offset, size) {
-    const remainder = offset % size
+    const remainder = offset % size;
 
     if (remainder === 0) {
-      return 0
+      return 0;
     }
 
-    return size - remainder
+    return size - remainder;
   }
 }
 
-class StructView {
-  #buffer
-  #byteLength
-  #byteOffset
+export class StructView {
+  #buffer;
+  #byteLength;
+  #byteOffset;
 
   get buffer() {
-    return this.#buffer
+    return this.#buffer;
   }
 
   get byteLength() {
-    return this.#byteLength
+    return this.#byteLength;
   }
 
   get byteOffset() {
-    return this.#byteOffset
+    return this.#byteOffset;
   }
 
   constructor(buffer, byteLength, offset) {
-    this.#buffer = buffer
-    this.#byteLength = byteLength
-    this.#byteOffset = offset
+    this.#buffer = buffer;
+    this.#byteLength = byteLength;
+    this.#byteOffset = offset;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
