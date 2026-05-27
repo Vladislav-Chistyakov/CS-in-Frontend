4606f9675bae8a3f99dbaff65711ad517540e3fa
Vladislav-Chistyakov <vladischis@gmail.com>
2026-05-27 17:43:35 +0300

    10: fix

--- 10/index.js
+++ 10/index.js
@@ -36,17 +36,24 @@ class Deque {
   }
   
   shift () {
-    const result = this.#startNode.shift()
-    if (this.#startNode.length === 0) {
+    if (this.length === 0) {
+      console.error('Error, this deque is empty')
+      return
+    }
+    
+    if (this.#startNode.arrayIsEmpty) {
       this.#startNode = this.#startNode.next
       this.#startNode.prev = undefined
+      this.#length --
+      return this.#startNode.shift()
+    } else {
+      this.#length --
+      return this.#startNode.shift()
     }
-    this.#length --
-    return result
   }
 
   unshift(value) {
-    if (this.#startNode.length === this.#startNode.capacity) {
+    if (this.#startNode.isStart) {
       const newNode = this.createNode()
       newNode.next = this.#startNode
       this.#startNode.prev = newNode
@@ -59,11 +66,19 @@ class Deque {
   }
 
   pop() {
-    if (this.#endNode.length === 0) {
+    if (this.length === 0) {
+      console.error('Error, this deque is empty')
+      return 
+    }
+    
+    console.log("CHECK ", this.#endNode.startIndex)
+    
+    if (this.#endNode.endIndex === 0) {
+      const result = this.#endNode.pop()
       this.#endNode = this.#endNode.prev
       this.#endNode.next = undefined
       this.#length --
-      return this.#endNode.pop()
+      return result
     } else {
       this.#length --
       return this.#endNode.pop()
@@ -71,14 +86,15 @@ class Deque {
   }
 
   push(value) {
-    if (this.#endNode.length !== this.#endNode.capacity) {
-      this.#endNode.push(value)
-    } else {
+    console.log('TTT ', this.#endNode.isEnd)
+    if (this.#endNode.isEnd) {
       const newNode = this.createNode()
       newNode.push(value)
       this.#endNode.next = newNode
       newNode.prev = this.#endNode
       this.#endNode = newNode
+    } else {
+      this.#endNode.push(value)
     }
     this.#length ++
   }
@@ -93,7 +109,22 @@ class Node {
   #buffer
   #length = 0
   #capacity = 0
+  #arrayIsFull = false
+  startIndex = null
+  endIndex = null
+
+  get arrayIsFull () {
+    return this.#arrayIsFull
+  }
   
+  get isEnd () {
+    return this.endIndex === this.capacity - 1
+  }
+
+  get isStart () {
+    return this.startIndex === 0
+  }
+
   get next () {
     return this.NEXT ? this.NEXT : undefined
   }
@@ -130,39 +161,112 @@ class Node {
     this.#size = size
   }
 
-  pop() {
-    this.#length -= 1
-    const result = this.ARRAY[this.#length]
-    this.ARRAY[this.#length] = 0
-    return result
+  checkArrayIsEmpty () {
+    this.#arrayIsFull = this.#length === 0;
   }
   
-  push(value) {
-    this.ARRAY[this.#length] = value
-    this.#length += 1
-  }
+  pop() {
+    const result = this.ARRAY[this.endIndex]
+    this.ARRAY[this.endIndex] = 0
+    this.endIndex -= 1
 
-  unshift(value) {
-    this.ARRAY[(this.#capacity - this.#length) - 1] = value
-    this.#length ++
+    this.#length -= 1
+    this.checkArrayIsEmpty()
+    return result
   }
 
   shift() {
-    const result  = this.ARRAY[this.#capacity - this.#length]
-    this.ARRAY[this.#capacity - this.#length] = 0
-    this.#length --
+    const result  = this.ARRAY[this.startIndex]
+    this.ARRAY[this.startIndex] = 0
+    this.startIndex += 1
+
+    this.#length -= 1
+    this.checkArrayIsFull()
     return result
+  }
+
+  checkArrayIsFull () {
+    this.#arrayIsFull = this.#capacity === this.#length;
+  }
+  
+  push(value) {
+    if (this.endIndex === null && this.startIndex === null) {
+      this.endIndex = 0
+      this.startIndex = 0
+      this.ARRAY[this.endIndex] = value
+    } else {
+      if (!this.isEnd) {
+        this.ARRAY[this.endIndex + 1] = value
+        this.endIndex += 1
+      }
+    }
+    
+    this.#length += 1
+    this.checkArrayIsFull()
+  }
+
+  unshift(value) {
+    if (this.endIndex === null && this.startIndex === null) {
+      this.endIndex = this.capacity - 1
+      this.startIndex = this.capacity - 1
+      this.ARRAY[this.startIndex] = value
+    } else {
+      if (this.startIndex !== 0) {
+        this.ARRAY[this.startIndex] = value
+        this.startIndex -= 1
+      } else {
+        this.ARRAY[this.startIndex] = value
+      }
+    }
+    
+    this.#length += 1
+    this.checkArrayIsFull()
   }
 }
 
-const deque = new Deque(Uint8Array, 2);
+const deque = new Deque(Uint8Array, 3);
 deque.push(12)
 deque.push(13)
-deque.push(14)
-console.log('Deque', deque.startNode)
 deque.unshift(11)
-deque.unshift(10)
-deque.unshift(9)
-deque.shift()
-console.log('Deque', deque.startNode)
+deque.push(14)
+deque.push(15)
+console.log('Length', deque.startNode, deque.endNode)
+
+// TODO Не двигается start index
+
+// сделать push самое начало, а unshift конец
+// push(1)
+// [1, 0, 0]
+// unshift(1)
+// [0, 0, 1]  [1, 0, 0]
+// push (2)
+// [0, 0, 1]  [1, 2, 0]
+// shift() // 1
+// [1, 2, 0]
+// push(3)
+// [1, 2, 3]
+// shift() // 1
+// [0, 2, 3]
+// push(4)
+// [0, 2, 3] [4, 0, 0]
+// unshift(1)
+// [1, 2, 3] [4, 0, 0]
+// unshift(9)
+// [0, 0, 9] [1, 2, 3] [4, 0, 0]
+// pop() // 4
+// [0, 0, 9] [1, 2, 3]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
