479f40644bd582cbfb2bd4fabe2b915a254dd31f
Vladislav-Chistyakov <vladischis@gmail.com>
2026-06-01 23:03:27 +0300

    11: Базовая реализация deref и pointer

--- 11/index.js
+++ 11/index.js
@@ -16,18 +16,95 @@ class Memory {
     this.dataView.setUint8(0, 3)
 
     if (options && options.stack && typeof options.stack === 'number') {
-      this.dataView.setUint8(0, 7)
-      
-      this.STACK = new DataView(this.#arrayBuffer, 0, options.stack)
-
-      this.dataView.setUint8(1, 17)
-      console.log('STACK', this.STACK);
+      this.STACK = new Stack(this.#arrayBuffer, options.stack)
     }
-    
-    console.log('LENGTH', this.#arrayBuffer);
+  }
+
+  push (buffer) {
+    return this.STACK.push(buffer)
+  }
+
+  get bufferStack () {
+    return this.STACK.bufferStack
   }
 }
-const mem = new Memory(10 * 1024, { stack: 1024 });
+
+class Stack {
+  #pointer = 0
+  #buffer
+  #dataView
+  #endMemory
+
+  get view () {
+    return this.#dataView
+  }
+
+  get pointer () {
+    return this.#pointer
+  }
+
+  constructor(arrayBuffer, endMemory) {
+    // Збесь на первом этапе мы записываем всю память что нам позволяют взять
+    this.#buffer = arrayBuffer
+    this.#dataView = new DataView(arrayBuffer, 0, endMemory)
+    // endMemory последний байт памяти
+    this.#endMemory = endMemory
+  }
+
+  push (buffer) {
+    const oldBuffer = new Uint8Array(this.#buffer, this.#pointer)
+    const newBuffer = new Uint8Array(buffer)
+    
+    // TODO вернуть указатель на первый байт
+    oldBuffer.set(newBuffer)
+    
+    // сохранить его
+    const startPointer = this.#pointer
+    
+    // Перезаписали указатель, он ссылка на пустое место в нашем буффере для следующей записи
+    this.#pointer = this.#pointer + newBuffer.byteLength
+    
+    // Отдали указатель на начало
+    return new PointerStack(this.#buffer, startPointer, newBuffer.length)
+  }
+  
+  get bufferStack () {
+    return this.#buffer
+  }
+}
+
+class PointerStack {
+  #buffer
+  #pointerStart
+  #bufferLength
+  
+  constructor(buffer, pointerStart, bufferLength) {
+    this.#buffer = buffer
+    this.#pointerStart = pointerStart
+    this.#bufferLength = bufferLength
+  }
+  
+  deref() {
+    return this.#buffer.slice(this.#pointerStart, this.#bufferLength)
+  }
+}
+
+const arrayBuffer1 = new Uint32Array([1234567]).buffer;
+const arrayBuffer2 = new Uint8Array([1, 2, 3, 4, 5]).buffer;
+const arrayBuffer3 = new Uint16Array([17, 9]).buffer;
+const arrayBuffer4 = new Uint8Array([8, 9, 7, 5]).buffer;
+
+const memory = new Memory(10 * 1024, { stack: 1024 });
+
+// console.log('T - ', arrayBuffer1.byteLength)
+// console.log('T - ', memory.push(arrayBuffer1))
+// console.log('T - ', memory.push(arrayBuffer2))
+// console.log('T - ', memory.bufferStack)
+
+const pointer1 = memory.push(arrayBuffer1)
+console.log('pointer1 - ', pointer1.deref())
+
+
 
 // ============================================
 //  Работа со стеком (LIFO)
