9fd99ce3ddf51ffa05c9e9ea9c54909e78590352
Vladislav-Chistyakov <vladischis@gmail.com>
2026-07-25 10:50:53 +0300

    15: Начал traverse

--- 15/index.js
+++ 15/index.js
@@ -34,12 +34,17 @@ class Matrix {
   checkArray() {
     console.log('ARR ', this.#arrayMatrix)
   }
+  
+  get width () {
+    return this.#width
+  }
 }
 
 const matrix = new Matrix(Uint8Array, 10, 10);  // 10×10, значения 0-255
 
 class Graph {
   #matrix
+  #boundariesPassed = []
   
   constructor(matrix) {
     this.#matrix = matrix
@@ -74,19 +79,60 @@ class Graph {
   checkMatrix () {
     this.#matrix.checkArray()
   }
+
+  traverse (row, callback) {
+    this.bypassingConnections(row, callback, 1)
+    this.#boundariesPassed = []
+  }
+
+  bypassingConnections (row, callback, countDepth) {
+    // Цикл заканчивается когда мы прошлись по всему элементу, связи закончились
+
+    // TODO Переписать на set
+    for (let i = 0; i < this.#matrix.width; i++) {
+      // Если элемент вершины имеет связь с i то заходим
+      if (this.hasArc(row, i) && !this.checkItemBoundariesPassed(row)) {
+        console.log('ROW ', row, ' Col ', i)
+        this.#boundariesPassed.push(row)
+
+        callback({ id: i, weight: this.#matrix.get(row, i)}, countDepth)
+
+        this.bypassingConnections(i, callback, countDepth + 1)
+      }
+    }
+
+    // console.log('this.#boundariesPassed ', this.#boundariesPassed)
+  }
+
+  // Это функция для проверки элемента в BoundariesPassed
+  // Если элемент есть, то выводим правду, иначе лож
+  checkItemBoundariesPassed (row) {
+    return this.#boundariesPassed.some((item) => item === row)
+  }
 }
 
 // Заполняем матрицу смежности
 const graph = new Graph(matrix);
 
 // Добавляем ребро между двумя узлами (с опциональным весом)
-graph.addArc(7, 2, 1);
+graph.addEdge(7, 2, 72);
+graph.addEdge(1, 2, 12);
+graph.addEdge(1, 3, 13);
+// graph.addEdge(2, 3, 23);
+graph.addEdge(2, 4, 24);
+graph.addEdge(2, 6, 26);
+graph.addEdge(3, 5, 35);
+graph.addEdge(3, 7, 37);
 
 // Проверяем смежность двух узлов (с учётом направленности)
 console.log('edge ', graph.hasEdge(7, 2));      // Для неориентированного графа
 console.log('arc ', graph.hasArc(7, 2));       // Для ориентированного графа (направление важно)
 console.log('arc ', graph.hasArc(2, 7));       // Для ориентированного графа (направление важно)
 graph.checkMatrix()
+
+graph.traverse(1, (node, depth) => {
+  console.log(`Узел: ${node.id}, глубина: ${depth}, вес ребра: ${node.weight}`);
+});
 
 
 // Удаляем ребро между двумя узлами
